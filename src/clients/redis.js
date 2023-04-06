31bc6889d40c44807456b2aad3e50187f45b4baf
elifhelincarboga <elifhelincarboga@gmail.com>
2023-04-06 23:16:37 +0300

    app init

---
+++ src/clients/redis.js
@@ -0,0 +1,4 @@
+const Redis = require("ioredis");
+const redis = new Redis("redis://default:sArTs83PBquBPj1m175zwmK7ic0X7qhJ@redis-19381.c293.eu-central-1-1.ec2.cloud.redislabs.com:19381");
+
+export default redis;
